d7dc91b9f906cdec175f1ac2c1e02334bce7a5a3
PAG1969 <paul@goodmanconstruction.co.uk>
2026-09-24 16:19:09 +0100

    Extend end date and update current holdings date

--- Axa_global_tech_Nav_dashboard.py
+++ Axa_global_tech_Nav_dashboard.py
@@ -145,7 +145,7 @@ def run_portfolio_system():
   st.subheader("Model Execution & Performance Engine")
 
   start_date = "2026-07-01"
-  end_date = "2026-09-23"  # Extended to include today's settled close
+  end_date = "2026-09-25"  # Extended to include today's settled close
 
   actual_dates = [
       "2026-07-13",
@@ -199,6 +199,8 @@ def run_portfolio_system():
       "2026-09-18",
       "2026-09-21",
       "2026-09-22",
+      "2026-09-23",
+      "2026-09-24",
   ]
 
   actual_nav_path = [
@@ -253,6 +255,8 @@ def run_portfolio_system():
       4.190,
       4.250,
       4.380,
+      4.440,
+      4.4420
   ]
 
   # --- SIDEBAR WIDGETS FOR DYNAMIC CONTROL ---
@@ -268,7 +272,7 @@ def run_portfolio_system():
 
   past_holdings = get_top_10_holdings(pd.to_datetime("2026-07-15"))
   aug_holdings = get_top_10_holdings(pd.to_datetime("2026-08-15"))
-  current_holdings = get_top_10_holdings(pd.to_datetime("2026-09-22"))
+  current_holdings = get_top_10_holdings(pd.to_datetime("2026-09-24"))
 
   tickers_list = list(
       set(
